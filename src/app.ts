fbf97cd88a1f6385527979520024d46cac1a6a54
Ibarra <ibarra2002x3@gmail.com>
2024-02-23 00:35:58 -0600

    commit

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
-import "dotenv/config"
-import express from "express"
-import cors from "cors"
-import routes from "./infrastructure/router"
-const port = process.env.PORT || 3002
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.use(express.static('tmp'))
-app.use(`/`,routes)
+import "dotenv/config";
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import routes from "./infrastructure/router";
+const port = process.env.PORT || 3002;
+const app = express();
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.static("tmp"));
+app.use(`/`, routes);
 
-app.listen(port, () => console.log(`Ready...${port}`))
+app.listen(port, () => console.log(`Ready...${port}`));
